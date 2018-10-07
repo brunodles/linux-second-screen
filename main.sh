dc31ee885836cdfd034c906518ed0f52e8a2d35b
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-10-07 05:06:25 -0300

    Add function to calculate resolution on main.sh

--- main.sh
+++ main.sh
@@ -12,6 +12,9 @@ main() {
     setRes)
       setRes ${@:2}
     ;;
+    calcRes)
+      calcRes ${@:2}
+    ;;
     *)
       help
     ;;
@@ -61,6 +64,35 @@ setRes() {
   xrandr --output $virtual --mode $modeName --auto
 }
 
+calcRes() {
+  v_width=$1
+  v_height=$2
+  v_inches=$3
+
+  f_xrandr=$(xrandr | grep primary)
+  f_res=$(echo $f_xrandr | cut -d' ' -f 4)
+  f_res=(${f_res//[+x]/ })
+  f_dimens=$(echo $f_xrandr | cut -d' ' -f 13,15)
+  f_dimens=(${f_dimens//m/})
+  
+  f_width=${f_res[0]}
+  f_height=${f_res[1]}
+  f_inches=$(bc <<< "scale=2; sqrt( ${f_dimens[0]}^2 + ${f_dimens[1]}^2 ) * 0.0393701")
+
+  proportion=$(bc <<< "scale=4; $v_inches / $f_inches")
+
+  r_width=$(bc <<< "scale=0; $v_width * $proportion")
+  r_height=$(bc <<< "scale=0; $v_height * $proportion")
+
+  echo Proportion $proportion
+  echo "        Width x Height Inches"
+  echo "fisical  $f_width x $f_height $f_inches"
+  echo "virtual  $v_width x $v_height $v_inches"
+  echo "----------------------------"
+  echo " result  $r_width x $r_height"
+
+}
+
 help() {
   cat <<TEXT
 Virtual Display Helper
@@ -73,6 +105,10 @@ Commands
  setRes <display> <width> <height>
   - set resolution for a virtual display
     example: $0 setRes VIRTUAL1 800 600
+
+ calcRes <width> <heigh> <inches>
+  - calculate possible resolutions for a device.
+    As devices may have same or  even greater resolutions compared to a normal display, de idea is to use a lower resolution for this device. This leads to a better visualization and performance.
 
  help
   - show this message
