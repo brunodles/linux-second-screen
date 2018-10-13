3282f55d0853704e4eccf56145db14deb567c12d
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-10-13 00:05:31 -0300

    Update droid script to include network commands

--- droid.sh
+++ droid.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
 setDate() {
-  command="adb shell su -c \"date -s $1\" 0"
-  echo $command
-  $command
+  su date -s $1
+}
+
+getIp() {
+#  run "adb shell ip a"
+  shell netcfg
+}
+
+wifiConnect() {
+# TODO: may need to do manually, look https://stackoverflow.com/a/9368211/1622925
+  shell am startservice \
+    -n com.google.wifisetup/.WifiSetupService \
+    -a WifiSetupService.Connect \
+    -e ssid $1 \
+    -e passphrase $2
+}
+
+open() {
+  home() {
+    shell am start -a android.intent.action.MAIN -c android.intent.category.HOME
+  }
+  $1
+}
+
+su() {
+  shell su -c \"$@\" 0
+}
+
+shell() {
+  run adb shell $@
+}
+
+run() {
+  echo \>$@
+  $@
 }
 
 help() {
@@ -11,15 +43,17 @@ help() {
 Droid Commands
  setDate <date format>
   - change device date using format yyyyMMdd.HHmmss
+
+ getIp
+  - Print all IPs for device
+
+ wifiConnect <ssid> <pasword>
+  - Connectos to a wifi network
 TEXT
 }
 
-case $1 in
-  setDate)
-    $1 ${@:2}
-  ;;
-  *)
-    help
-  ;;
-esac
-#echo $(type -t $1)
+if [ "$(type -t $1)" == "function" ]; then
+  $@
+else
+  help
+fi
