bc7460368f63dbe8ce5e744d770227ea18895074
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-10-13 00:06:17 -0300

    Create adbKeyboard script

---
+++ adbKeyboard.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+. ./adbKeyboard_keycodes
+
+keycode() {
+  mode=$(( $2 + 1 ))
+  case $mode in
+    1)
+      code=$(eval echo \$$1)
+    ;;
+    2)
+      code=$(eval echo \$KEYCODE_$1)
+    ;;
+    3)
+      code=$(eval echo \$KEYCODE_DPAD_$1)
+    ;;
+    *)
+      echo ""
+      return 1
+    ;;
+  esac
+
+#  if [[ $code =~ ^-?[0-9]+$ && "$i" -lt "100" ]]; then
+  if [[ $code =~ ^-?[0-9]+$ ]]; then
+    echo $code
+    return 0
+  fi
+  echo $(keycode $1 $mode)
+}
+
+text=$@
+# String.replace " ", "%s"
+text=${text// /%s}
+
+# String.split ";"
+array=${text//;/ }
+
+for i in $array; do
+  code=$(keycode $i)
+  if [ -z "$code" ]; then
+    echo "text\>$i"
+    adb shell input keyevent $i
+  else
+    echo " key\>$code"
+    adb shell input text "$i"
+  fi
+done
