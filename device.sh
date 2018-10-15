602d9967c5f6384f0466feb5ed8773fc21fbe66e
Bruno de Lima e Silva <dlimaun@gmail.com>
2018-10-15 12:32:40 -0300

    Create device target wrapper script

---
+++ device.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+CONFIG_FILE=".device.config"
+
+if [ -f "$CONFIG_FILE" ]; then
+  source $CONFIG_FILE
+fi
+
+case $1 in
+  add)
+    echo "$2=$3">>$CONFIG_FILE
+  ;;
+  *h|*help)
+    cat <<TEXT
+Device helper
+  A simple wrapper to dive devices alias
+
+usage:
+  $0 add <alias> <serial>
+    - Add device to a alias list
+
+  $0 <alias> <command>
+    - run command on given device
+
+  $0 h
+  $0 help
+    - Show this message
+TEXT
+  ;;
+  *)
+    ANDROID_SERIAL=$(eval echo \$$1)
+    echo Serial \"$ANDROID_SERIAL\"
+    ${@:2}
+esac
